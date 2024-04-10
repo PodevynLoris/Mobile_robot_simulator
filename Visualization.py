b3fa855d4a3e321b2e9b5263b4dd04595dc89bd6
Benjamin Gauthier <b.gauthier@student.maastrichtuniversity.nl>
2024-04-10 15:12:11 +0200

    hey

---
+++ Visualization.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+robot = pygame.C
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
